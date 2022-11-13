9245edb138908a13208ccd34f6b0d89a3c54ebea
shabrina salsabila kurniawan <shabrinasalsabila15@gmail.com>
2022-11-13 13:23:54 +0700

    feat: add RumahSehat initialize spring boot

---
+++ RumahSehat/src/main/java/apap/tugas/akhir/RumahSehat/RumahSehatApplication.java
@@ -0,0 +1,13 @@
+package apap.tugas.akhir.RumahSehat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahSehatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahSehatApplication.class, args);
+	}
+
+}
---
+++ RumahSehat/src/test/java/apap/tugas/akhir/RumahSehat/RumahSehatApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugas.akhir.RumahSehat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahSehatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
