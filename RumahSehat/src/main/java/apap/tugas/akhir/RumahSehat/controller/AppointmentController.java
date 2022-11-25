1d68a11f12dc78dfd0cff280b73dacaf40d952d7
Shabrina Taqiyya <shabrina.taqiyya@ui.ac.id>
2022-11-25 20:14:47 +0700

    feat/updateMelihatDetailAppointment

--- RumahSehat/src/main/java/apap/tugas/akhir/RumahSehat/controller/AppointmentController.java
+++ RumahSehat/src/main/java/apap/tugas/akhir/RumahSehat/controller/AppointmentController.java
@@ -4,6 +4,7 @@ import apap.tugas.akhir.RumahSehat.model.AppointmentModel;
 import apap.tugas.akhir.RumahSehat.model.DokterModel;
 import apap.tugas.akhir.RumahSehat.model.PasienModel;
 import apap.tugas.akhir.RumahSehat.model.ResepModel;
+import apap.tugas.akhir.RumahSehat.model.TagihanModel;
 import apap.tugas.akhir.RumahSehat.service.*;
 
 import java.util.List;
@@ -30,22 +31,45 @@ public class AppointmentController {
         // List<AppointmentModel> listAppointmentDokter = ;
         // model.addAttribute("jadwalAppointment", jadwalAppointment);
         model.addAttribute("listAppointment", listAppointment);
+        
         return "viewall-appointment";
     }
 
     @GetMapping("/appointment/{kode}")
     public String viewDetailAppointmentPage(@PathVariable String kode, Model model) {
         AppointmentModel appointment = appointmentService.getAppointmentByKode(kode);
+        // Counter jumlah resep yg belum selesai
+        int counter = 0;
         for (ResepModel resep : appointment.getListResep()) {
-            if (resep.getIsDone()) {
-                
+            if (!resep.getIsDone()) {
+                counter++;
             }
         }
+        // System.out.printn("masukkk");
+        // System.out.printlln(counter);
+        // Artinya ada resep yg blm selesai shg gabisa selesaiin appointment
+        // if (counter > 0) {
+        // model.addAttribute("counter", counter);
+        // }
+        // Yg belum itu yg ada resepnya, tp udh selesai semua
+        // tombol selesainya gk muncul
         if (!appointment.getIsDone()) {
             model.addAttribute("appointment", appointment);
             return "view-appointment";
         }
         model.addAttribute("appointment", appointment);
+        model.addAttribute("counter", counter);
         return "view-appointment-done";
+    }
+
+    @GetMapping("/appointment/done/{kode}")
+    public String updateDoneAppointment(@PathVariable String kode, Model model) {
+        AppointmentModel appointment = appointmentService.getAppointmentByKode(kode);
+        appointment.setIsDone(true);
+        appointmentService.setDoneAppointment(appointment);
+        TagihanModel tagihan = appointmentService.setTagihan(appointment);
+        model.addAttribute("kode", appointment.getKode());
+        model.addAttribute("tagihan", tagihan);
+        return "appointment-done";
     }
 }
--- RumahSehat/src/main/java/apap/tugas/akhir/RumahSehat/service/AppointmentService.java
+++ RumahSehat/src/main/java/apap/tugas/akhir/RumahSehat/service/AppointmentService.java
@@ -3,9 +3,11 @@ package apap.tugas.akhir.RumahSehat.service;
 import java.util.List;
 
 import apap.tugas.akhir.RumahSehat.model.AppointmentModel;
+import apap.tugas.akhir.RumahSehat.model.TagihanModel;
 
 public interface AppointmentService {
     List<AppointmentModel> getListAppointment();
     AppointmentModel getAppointmentByKode(String kode);
-    
+    AppointmentModel setDoneAppointment(AppointmentModel appointment);
+    TagihanModel setTagihan(AppointmentModel appointment);
 }
