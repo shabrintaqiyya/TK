4777a8e402b34726f17e093a9636421b3c17b745
Muhammad Raffy Suprajeni <muhammad.raffy@ui.ac.id>
2022-12-09 16:57:30 +0700

    Feat/fitur15

--- RumahSehat/src/main/java/apap/tugas/akhir/RumahSehat/controller/PasienController.java
+++ RumahSehat/src/main/java/apap/tugas/akhir/RumahSehat/controller/PasienController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class PasienController {
 
@@ -19,6 +21,29 @@ public class PasienController {
         PasienModel pasien = pasienService.getPasienById(id);
         model.addAttribute("pasien", pasien);
         return "view-pasien";
+    }
+
+    @GetMapping("/pasien/{id}/topup")
+    public String topupSaldoPasiemForm(@PathVariable String id, Model model){
+        PasienModel pasien = pasienService.getPasienById(id);
+        model.addAttribute("pasien", pasien);
+
+        return "form-topup-pasien";
+    }
+
+    @PostMapping("/pasien/{id}/topup")
+    public String topupSaldoPasienFormSubmit(@ModelAttribute PasienModel pasien, Model model){
+        // Hitung saldo baru
+        int saldoTambahan = pasien.getSaldo();
+        PasienModel tempPasien = pasienService.getPasienById(pasien.getId()); // Ambil objek pasien dari db
+        int saldoSaatIni = tempPasien.getSaldo();
+        int saldoBaru = saldoTambahan + saldoSaatIni;
+
+        tempPasien.setSaldo(saldoBaru);
+        PasienModel updatedPasien = pasienService.updatePasien(tempPasien); // Update pasien di db dengan pasien saldo baru
+        model.addAttribute("pasien", updatedPasien);
+
+        return "topup-pasien";
     }
 
 }
